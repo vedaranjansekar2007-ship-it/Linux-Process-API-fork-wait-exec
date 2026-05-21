dda80cf0cb083d8f1425d9b7724c36cb6f770fde
vedaranjan <vedaranjansekar2007@gmail.com>
2026-05-21 06:20:03 +0200

    [catfile]

---
+++ cprogram1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<sys/wait.h>
+#include <unistd.h>
+int main()
+{
+    int pid = fork();
+    if (pid == 0)
+     { 
+        printf("I am child, my PID is %d\n", getpid()); 
+        printf("My parent PID is: %d\n", getppid()); 
+        sleep(2);  // Keep child alive for verification
+    } else
+     { 
+        printf("I am parent, my PID is %d\n", getpid()); 
+        wait(NULL); 
+    }
+}
